778ee4f6adb6c3057adab0bcfc4627d2b7055ff9
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2023-08-21 10:28:07 +0100

    stash 0

---
+++ src/main.js
@@ -0,0 +1,63 @@
+import "./style.css";
+
+const root = document.querySelector('#root')
+const gameboardArr = Array.from({ length: 9 }, (_, index) => ({ mark: "", index }))
+const players = [
+    { name: 'P1', isHuman: true, mark: "x" },
+    { name: 'P2', isHuman: true, mark: "o" }
+]
+
+let currentPlayer = players[1]
+
+
+const renderGameboard = (arr, root) => {
+    const element = document.createElement('div')
+    element.classList.add('gameboard')
+    element.innerHTML = ''
+    
+    arr.forEach(cell => {
+        element.appendChild(createGameboardCell(cell))
+    })
+
+    return root.appendChild(element)
+}
+
+const createGameboardCell = (cell) => {
+    const element = document.createElement('div')
+    element.classList.add('cell')
+    element.dataset.mark = cell.mark
+    element.dataset.index = cell.index
+
+    element.tabIndex = "0"
+
+    return element
+}
+
+const computerChoice = () => {
+}
+
+const gameController = () => {
+    changePlayer()
+    if (currentPlayer.isHuman) {
+            const cells = Array.from(document
+            .querySelectorAll('.cell'))
+
+        cells.forEach(cell => 
+            cell.addEventListener('click', (e) => {
+                console.log('PC')
+                gameController()
+            })
+        )
+    }
+}
+
+const changePlayer = () => {
+    currentPlayer === players[0]
+    ? currentPlayer = players[1]
+    : currentPlayer = players[0]
+}
+
+
+
+renderGameboard(gameboardArr, root)
+gameController()
---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
