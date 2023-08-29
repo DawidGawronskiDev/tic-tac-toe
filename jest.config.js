fb650adb88cb00a3573ec90f07772ccd6c90023b
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2023-08-29 16:45:43 +0100

    Add site

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // Optional: Configure aliases for your webpack paths
-  },
-  transform: {
-    "^.+\\.jsx?$": "babel-jest", // Process JavaScript files with Babel
-  },
-};
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-import { gameSetup } from "./game/game";
-
-const root = document.querySelector("#root");
-
-gameSetup();
